e9c8bed6d6e386a074548f6c6523e817437cc31d
ahipoki <50777654+ahipoki@users.noreply.github.com>
2021-05-17 11:04:22 -0700

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -12,54 +12,54 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Minesweeper implements ActionListener{
-    JFrame frame = new JFrame("Minesweeper");
-    JButton reset = new JButton("Reset");
-    JButton[][] buttons = new JButton[20][20];
-    int[][] counts = new int[20][20];
-    Container grid = new Container();
-    final int MINE = 10;
+    JFrame frame = new JFrame("Minesweeper");//New jframe
+    JButton reset = new JButton("Reset");//reset button
+    JButton[][] buttons = new JButton[20][20];//grid of buttons
+    int[][] counts = new int[20][20];//counts
+    Container grid = new Container();//container for the grid
+    final int MINE = 10;//mine
 
     public Minesweeper(){
-        frame.setSize(1000, 700);
-        frame.setLayout(new BorderLayout());
-        frame.add(reset, BorderLayout.NORTH);
-        reset.addActionListener(this);
-        grid.setLayout(new GridLayout(20,20));
-        for (int i = 0; i < buttons.length; i++){
-            for (int j = 0; j < buttons[0].length; j++){
-                buttons[i][j] = new JButton();
-                buttons[i][j].addActionListener(this);
-                grid.add(buttons[i][j]);
+        frame.setSize(1000, 700);//set the frame size
+        frame.setLayout(new BorderLayout());//set the layout
+        frame.add(reset, BorderLayout.NORTH);//add the reset button to the top
+        reset.addActionListener(this);//add an actionlistener
+        grid.setLayout(new GridLayout(20,20));//set the layout of the grid
+        for (int i = 0; i < buttons.length; i++){//Loop through length of buttons
+            for (int j = 0; j < buttons[0].length; j++){//loop through length of buttons[0]
+                buttons[i][j] = new JButton();//new buttons
+                buttons[i][j].addActionListener(this);//add action listeners to each one
+                grid.add(buttons[i][j]);//add the buttons to the grid
             }
         }
-        frame.add(grid, BorderLayout.CENTER);
-        createRandomMines();
+        frame.add(grid, BorderLayout.CENTER);//add the grid of buttons to the center
+        createRandomMines();//create the mines
     
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close
+        frame.setVisible(true);//make the frame visible
     }
 
-    public void createRandomMines(){
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int x = 0; x < counts.length; x++){
-            for (int y = 0; y < counts[0].length; y++){
-                list.add(x*100+y);
+    public void createRandomMines(){//create mines
+        ArrayList<Integer> list = new ArrayList<Integer>();//array list of integers
+        for (int x = 0; x < counts.length; x++){//loop through length of couts
+            for (int y = 0; y < counts[0].length; y++){//loop through length of counts[0]
+                list.add(x*100+y);//add to the list
             }
         }
-        counts = new int[20][20];
-        for (int i = 0; i < 30; i++){
-            int choice = (int)(Math.random()*list.size());
-            counts[list.get(choice)/100][list.get(choice)%100] = MINE;
-            list.remove(choice);
+        counts = new int[20][20];//20x20 grid
+        for (int i = 0; i < 30; i++){//loop through until total mines are placed
+            int choice = (int)(Math.random()*list.size());//random placement
+            counts[list.get(choice)/100][list.get(choice)%100] = MINE;//get squre it's placed on
+            list.remove(choice);//remove the mine from the list of options
         }
-        for (int x = 0; x < counts.length; x++){
-            for (int y = 0; y < counts[0].length; y++){
+        for (int x = 0; x < counts.length; x++){//loop through length of counts
+            for (int y = 0; y < counts[0].length; y++){//loop through length of counts[0]
                 if (counts[x][y] != MINE){//If the current spot isn't a mine, check for mines around it
                     int neighborcount = 0;//# of mines
                     if (x > 0 && y > 0 && counts[x-1][y-1] == MINE){//Upper left
                         neighborcount++;//Add 1 to mine count
                     }
-                    if (y > 0 && counts[x][y-1] == MINE){//Up
+                    if (y > 0 && counts[x][y-1] == MINE){//top
                         neighborcount++;//Add 1 to mine count
                     }
                     if (x < counts.length-1 && y > 0 && counts[x+1][y-1] == MINE){//upper right
@@ -74,7 +74,7 @@ public class Minesweeper implements ActionListener{
                     if (x > 0 && y < counts[0].length-1 && counts[x-1][y+1] == MINE){//bottom left
                         neighborcount++;
                     }
-                    if (y < counts[0].length-1 && counts[x][y+1] == MINE){//Down
+                    if (y < counts[0].length-1 && counts[x][y+1] == MINE){//bottom
                         neighborcount++;
                     }
                     if (x < counts.length-1 && y < counts[0].length-1 && counts[x+1][y+1] == MINE){//bottom right
@@ -86,31 +86,31 @@ public class Minesweeper implements ActionListener{
         }
     }
   
-    public void lostGame(){
-        for (int x = 0; x < buttons.length; x++){
-            for (int y = 0; y < buttons[0].length; y++){
-                if (buttons[x][y].isEnabled()){
-                    if (counts[x][y] != MINE){
-                        buttons[x][y].setText(counts[x][y] + "");
-                        buttons[x][y].setEnabled(false);
+    public void lostGame(){//lost
+        for (int x = 0; x < buttons.length; x++){//loop through buttons length
+            for (int y = 0; y < buttons[0].length; y++){//loop through buttons[0] length
+                if (buttons[x][y].isEnabled()){//if the button is still enabled
+                    if (counts[x][y] != MINE){//if it's not a mine
+                        buttons[x][y].setText(counts[x][y] + "");//clear the square
+                        buttons[x][y].setEnabled(false);//disable the button
                     }
-                    else{
-                        buttons[x][y].setText("X");
-                        buttons[x][y].setEnabled(false);
+                    else{//anything else
+                        buttons[x][y].setText("X");//draw an x
+                        buttons[x][y].setEnabled(false);//disable the button
                     }
                 }
             }
         }
     }
   
-    public void clearZeros(ArrayList<Integer> toClear){
-        if (toClear.size() == 0){
-            return;
+    public void clearZeros(ArrayList<Integer> toClear){//clear zeros
+        if (toClear.size() == 0){//if there's none to clear
+            return;//return
         }
-        else{
-            int x = toClear.get(0)/100;
-            int y = toClear.get(0)%100;
-            toClear.remove(0);
+        else{//anything else
+            int x = toClear.get(0)/100;//x of what's needed to be cleared
+            int y = toClear.get(0)%100;//y of what's needed to be cleared
+            toClear.remove(0);//remove it
             if (x > 0 && y > 0 && buttons[x-1][y-1].isEnabled()){//upper left
                 buttons[x-1][y-1].setText(counts[x-1][y-1] + "");
                 buttons[x-1][y-1].setEnabled(false);
@@ -118,7 +118,7 @@ public class Minesweeper implements ActionListener{
                     toClear.add((x-1) * 100 + (y-1));
                 }
             }
-            if (y > 0 && buttons[x][y-1].isEnabled()){
+            if (y > 0 && buttons[x][y-1].isEnabled()){//top
                 buttons[x][y-1].setText(counts[x][y-1] + "");
                 buttons[x][y-1].setEnabled(false);
                 if (counts[x][y-1] == 0){
@@ -160,7 +160,7 @@ public class Minesweeper implements ActionListener{
                     toClear.add(x * 100 + (y+1));
                 }
             }
-            if (x < counts.length - 1 && y < counts[0].length - 1 && buttons[x+1][y+1].isEnabled()){
+            if (x < counts.length - 1 && y < counts[0].length - 1 && buttons[x+1][y+1].isEnabled()){//bottom right
                 buttons[x+1][y+1].setText(counts[x+1][y+1] + "");
                 buttons[x+1][y+1].setEnabled(false);
                 if (counts[x+1][y+1] == 0){
@@ -171,52 +171,52 @@ public class Minesweeper implements ActionListener{
         clearZeros(toClear);
     }
   
-    public void checkWin(){
-        boolean won = false;
-        for (int x = 0; x < counts.length; x++){
-            for (int y = 0; y < counts[0].length; y++){
-                if (counts[x][y] != MINE && buttons[x][y].isEnabled() == true){
-                    won = false;
+    public void checkWin(){//check for wins
+        boolean won = false;//not won yet
+        for (int x = 0; x < counts.length; x++){//loop through counts length
+            for (int y = 0; y < counts[0].length; y++){//loop through counts[0] length
+                if (counts[x][y] != MINE && buttons[x][y].isEnabled() == true){//if there's squares that aren't mines or disabled
+                    won = false;//not won yet
                 }
             }
         }
-        if (won == true){
-            JOptionPane.showMessageDialog(frame, "You Win!");
+        if (won == true){//if won
+            JOptionPane.showMessageDialog(frame, "You Win!");//you won
         }
     }
   
     @Override
-    public void actionPerformed(ActionEvent event){
-        if (event.getSource().equals(reset)){
-            for (int x = 0; x < buttons.length; x++){
-                for (int y = 0; y < buttons[0].length; y++){
-                    buttons[x][y].setEnabled(true);
-                    buttons[x][y].setText("");
+    public void actionPerformed(ActionEvent event){//action performed
+        if (event.getSource().equals(reset)){//if reset button is clicked
+            for (int x = 0; x < buttons.length; x++){//loop through buttons
+                for (int y = 0; y < buttons[0].length; y++){//loop through buttons[0]
+                    buttons[x][y].setEnabled(true);//enable buttons
+                    buttons[x][y].setText("");//clear them
                 }
             }
-            createRandomMines();
+            createRandomMines();//create random mines
         }
-        else{
-            for (int x = 0; x < buttons.length; x++){
-                for (int y = 0; y < buttons[0].length; y++){
-                    if (event.getSource().equals(buttons[x][y])){
-                        if (counts[x][y] == MINE){
-                            buttons[x][y].setForeground(Color.red);
-                            buttons[x][y].setText("X");
-                            lostGame();
+        else{//anything else
+            for (int x = 0; x < buttons.length; x++){//loop through buttons
+                for (int y = 0; y < buttons[0].length; y++){//loop through buttons[0]
+                    if (event.getSource().equals(buttons[x][y])){//if it's a button in play
+                        if (counts[x][y] == MINE){//if it's a mine
+                            buttons[x][y].setForeground(Color.red);//change color to red
+                            buttons[x][y].setText("X");//draw an x
+                            lostGame();//game over
                         }
-                        else if (counts[x][y] == 0){
-                            buttons[x][y].setText(counts[x][y] + "");
-                            buttons[x][y].setEnabled(false);
-                            ArrayList<Integer> toClear = new ArrayList<Integer>();
-                            toClear.add(x*100+y);
-                            clearZeros(toClear);
-                            checkWin();
+                        else if (counts[x][y] == 0){//if it's a 0
+                            buttons[x][y].setText(counts[x][y] + "");//draw a 0
+                            buttons[x][y].setEnabled(false);//disable the button
+                            ArrayList<Integer> toClear = new ArrayList<Integer>();//clear around it
+                            toClear.add(x*100+y);//add to clear
+                            clearZeros(toClear);//clear zeros
+                            checkWin();//check for wins
                         }
-                        else{
-                            buttons[x][y].setText(counts[x][y] + "");
-                            buttons[x][y].setEnabled(false);
-                            checkWin();
+                        else{//anything else
+                            buttons[x][y].setText(counts[x][y] + "");//draw # of mines around it
+                            buttons[x][y].setEnabled(false);//disable it
+                            checkWin();//check for wins
                         }
                     }
                 }
